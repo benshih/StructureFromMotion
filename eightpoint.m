cd7e70bd60b392a13fbdfe0c3b3ab294d79272cb
Benjamin Shih <benjshih@gmail.com>
2013-11-08 01:42:45 -0500

    don't think it fully works yet. epipolar lines don't appear as they do in handout

--- eightpoint.m
+++ eightpoint.m
@@ -3,10 +3,42 @@
 % Assignment 5 3D Reconstruction
 % Eight Point Algorithm
 
+% Computes the fundamental matrix based on input points from X and Y, with
+% scale M.
+
 function [ F ] = eightpoint( X, Y, M )
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-
-
+    xscale = X ./ M;
+    yscale = Y ./ M;
+    
+    % Nx1 dimensional coordinate points.
+    x = xscale(:,1);
+    xp = xscale(:,2);
+    y = yscale(:,1);
+    yp = yscale(:,2);
+    
+    numPts = size(xp, 1);
+    
+    % Generate the Nx9 A matrix.
+    A = [x.*xp, ...
+         x.*yp, ...
+         x, ...
+         y.*xp, ...
+         y.*yp,  ...
+         y,  ...
+         xp,  ...
+         yp,  ...
+         ones(numPts, 1)];
+    
+    % Compute the SVD of A
+    [U, S, V] = svd(A);
+    F = reshape(V(:,9), 3, 3)';
+    [FU, FS, FV] = svd(F);
+    FS(3,3) = 0;
+    F = FU*FS*FV';
+    
 end
 
+
+%% Useful for checking if the points are correct:
+% close; figure; imshow(im2);hold on; scatter(myBasePts(:,1), myBasePts(:,2))
+% close; figure; imshow(im1);hold on; scatter(myInputPts(:,1), myInputPts(:,2))
