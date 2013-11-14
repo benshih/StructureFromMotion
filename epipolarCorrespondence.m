2f4e3a746ee68e33a6ee6dce787ca003d0f927bf
Benjamin Shih <benjshih@gmail.com>
2013-11-14 18:43:47 -0500

    began epipolar correspondence computation part 3

---
+++ epipolarCorrespondence.m
@@ -0,0 +1,14 @@
+% Benjamin Shih
+% 16720F13 Computer Vision
+% Assignment 5 3D Reconstruction
+% Q3.1 3D Visualization
+
+% This function creates a 3D rendering of the courtyard by treating he two
+% images as a stereo-pair. This assumption allows ut so triangulate
+% corresponding points in each image, and render their 3D location. 
+
+function [ x2, y2 ] = epipolarCorrespondence( im1, im2, F, x1, y1 )
+    
+    
+    
+end
